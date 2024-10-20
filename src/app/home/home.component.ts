7c03fd24b1ac03adb7afa3ac0612050abf75e03d
Stuntmanbob1986 <stuntmanbob1986@web.de>
2024-10-20 22:29:41 +0200

    finished lesson6 - Add a property binding to a component's template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,17 +11,10 @@ import { HousingLocation } from '../housinglocation';
     HousingLocationComponent
   ],
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location></app-housing-location>
-    </section>
+    <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
   `,
   styleUrl: './home.component.css'
+
 })
 export class HomeComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
