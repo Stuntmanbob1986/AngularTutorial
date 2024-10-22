b161ac3aa7df7a1437477877975bb46f0e8876be
Stuntmanbob1986 <stuntmanbob1986@web.de>
2024-10-22 23:33:40 +0200

    finished lesson 12 - Adding a form to your angular app

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -110,14 +110,18 @@ export class HousingService {
     }
   ];
 
-getAllHousingLocations(): HousingLocation[] {
-  return this.housingLocationList;
-}
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
 
-getHousingLocationById(id: number): HousingLocation | undefined {
-  return this.housingLocationList.find(housingLocation =>
-    housingLocation.id === id);
-}
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation =>
+      housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
 
   constructor() { }
 }
